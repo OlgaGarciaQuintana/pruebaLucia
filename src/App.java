63ce80af273d49f239d6ed52eabd465add41fe63
Olga <olgarciaq11@gmail.com>
2025-05-21 13:04:51 +0200

    miau

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Holis, soy Lucia :3");
+
+        System.out.println("Hola Luciaaa, yo soy Olga <3");
+
     }
 }
